c8747943c24f019e8a946337a2d6ac31c289788f
ethan <anpufeng@gmail.com>
2015-07-12 09:47:54 +0800

    多线程特定任务顺序执行

---
+++ TestMultithreadingSerial/TestMultithreadingSerial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestMultithreadingSerial
+//
+//  Created by ethan on 15/7/11.
+//  Copyright (c) 2015年 ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
